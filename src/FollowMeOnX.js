bff847eac5fffdd4fe4912548dd04a9be9e6ca6e
Ronnel Boettcher <ronnel132@gmail.com>
2024-07-31 22:22:19 -0700

    fix twitter handle

--- src/FollowMeOnX.js
+++ src/FollowMeOnX.js
@@ -3,7 +3,7 @@ import x_profile from './assets/x_profile.jpg';
 
 const FollowMeOnX = () => {
   const handleClick = () => {
-    window.open('https://x.com/xetur132', '_blank');
+    window.open('https://x.com/xetur_xyz', '_blank');
   };
 
   return (
@@ -38,7 +38,7 @@ const FollowMeOnX = () => {
               marginRight: '10px',
             }}
           />
-          <span style={{ paddingTop: '16px', fontSize: '16px', color: 'white', fontWeight: 'bolder' }}>@xetur132</span> {/* Black color for the handle */}
+          <span style={{ paddingTop: '16px', fontSize: '16px', color: 'white', fontWeight: 'bolder' }}>@xetur_xyz</span> {/* Black color for the handle */}
         </div>
       </div>
     </div>
